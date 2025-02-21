7ecdcb0526b3f72b201aeae514ea021999e24221
ik willem <hwj.beltman@gmail.com>
2025-02-21 11:27:09 +0100

    Initial commit after moving to own repository

---
+++ BeltmanSoftwareDesign.StorageBlob.Business/Services/StorageFileService.cs
@@ -0,0 +1,17 @@
+﻿using StorageBlob.Proxy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageBlob.Proxy.Services
+{
+    public class StorageFileService : IStorageFileService
+    {
+        public string GetUrl(IStorageFile storageFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
